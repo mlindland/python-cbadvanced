43dc1860d2f92ee75ebd382dd04b3ba51f606a7c
Magnus <magnuslindland@protonmail.ch>
2025-06-20 22:29:26 -0600

    Formatting, updating cb auth to latest json web token

--- cbadvanced/cb_auth.py
+++ cbadvanced/cb_auth.py
@@ -1,30 +1,48 @@
-import hmac
-import hashlib
 import time
-
+import jwt
+from cryptography.hazmat.primitives import serialization
+import secrets
 from requests.auth import AuthBase
+from urllib.parse import urlparse
 
 
 class CBAuth(AuthBase):
-    def __init__(self, api_key, secret_key):
-        self.api_key = api_key
-        self.secret_key = secret_key
+    def __init__(self, key_name, key_secret):
+        self.key_name = key_name
+        self.key_secret = key_secret
 
     def __call__(self, request):
-        timestamp = str(int(time.time()))
-        message = ''.join([timestamp, request.method,
-                           request.path_url.split('?')[0], str(request.body or '')])
-        request.headers.update(get_auth_headers(timestamp, message,
-                                                self.api_key,
-                                                self.secret_key))
+        request.headers.update(
+            {
+                "Authorization": f"Bearer {self._build_jwt(self._get_uri_from_request(request))}"
+            }
+        )
         return request
 
+    def _get_uri_from_request(self, request):
+        """Returns Coinbase-compatible URI (method host/path) from a requests.Request object."""
+        method = request.method
+        parsed_url = urlparse(request.url)
+        host = parsed_url.netloc
+        path = parsed_url.path
+        return f"{method} {host}{path}"
 
-def get_auth_headers(timestamp, message, api_key, secret_key):
-    signature = hmac.new(secret_key.encode('utf-8'), message.encode('utf-8'), digestmod=hashlib.sha256).digest().hex()
-    return {
-        'Accept': 'Application/JSON',
-        'CB-ACCESS-KEY': api_key,
-        'CB-ACCESS-TIMESTAMP': timestamp,
-        'CB-ACCESS-SIGN': signature
-    }
+    def _build_jwt(self, uri):
+        private_key_bytes = self.key_secret.encode("utf-8")
+        private_key = serialization.load_pem_private_key(
+            private_key_bytes, password=None
+        )
+        jwt_payload = {
+            "sub": self.key_name,
+            "iss": "cdp",
+            "nbf": int(time.time()),
+            "exp": int(time.time()) + 120,
+            "uri": uri,
+        }
+        jwt_token = jwt.encode(
+            jwt_payload,
+            private_key,
+            algorithm="ES256",
+            headers={"kid": self.key_name, "nonce": secrets.token_hex()},
+        )
+        return jwt_token
